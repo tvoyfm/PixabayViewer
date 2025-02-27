1f124f3b383eb99765f0eb136448bb7d7b651615
Глеб Столярчук <stolyarchukgm@tech.bcs.ru>
2025-02-27 17:46:51 +0100

    Initial Commit

---
+++ PixabayViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PixabayViewer
+//
+//  Created by Глеб Столярчук on 27.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PixabayViewer/PixabayViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixabayViewerApp.swift
+//  PixabayViewer
+//
+//  Created by Глеб Столярчук on 27.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct PixabayViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
